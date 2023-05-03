45706dcce299934ff2fab59e53cea73cc2a0a9fd
deniscozma2019 <denis.cozma2015@gmail.com>
2023-05-03 18:48:27 +0300

    Initial commit

---
+++ src/main/java/LucrareRestApi/LucrareRestApi/LucrareRestApiApplication.java
@@ -0,0 +1,13 @@
+package LucrareRestApi.LucrareRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucrareRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucrareRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LucrareRestApi/LucrareRestApi/LucrareRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package LucrareRestApi.LucrareRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucrareRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
